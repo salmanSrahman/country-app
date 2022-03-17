b95f7fb55feb132bd86bf678b0c4f3112a082d38
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-17 14:28:36 +0600

    all routes set up

--- src/Components/CountryDetails/CountryDetails.js
+++ src/Components/CountryDetails/CountryDetails.js
@@ -4,9 +4,6 @@ import { useParams } from 'react-router-dom';
 const CountryDetails = () => {
 const params = useParams()
 console.log(params);
-
-
-
       return (
             <div>
                   
