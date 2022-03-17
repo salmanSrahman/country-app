a531f6d339c295814f1ebeadb17b9bb747f7545f
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-17 15:33:25 +0600

    all routes set up

--- src/Components/CountryDetails/CountryDetails.js
+++ src/Components/CountryDetails/CountryDetails.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Details from "../Details/Details";
 
 const CountryDetails = () => {
-const params = useParams()
-console.log(params);
-      return (
-            <div>
-                  
-            </div>
-      );
+  const [countries, setCountries] = useState([]);
+  const { countryName } = useParams();
+  console.log(countryName);
+
+  useEffect(() => {
+    const url = `https://restcountries.com/v3.1/name/${countryName}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  return (
+    <div>
+      {countries.map((country) => (
+        <Details country={country}></Details>
+      ))}
+    </div>
+  );
 };
 
 export default CountryDetails;
